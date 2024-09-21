f9a045321b3f2c93506ace3164b37071e502bf27
Kelvin <flamencycle2020@gmail>
2024-09-21 05:10:49 -0500

    Finish Product

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"math/rand"
 	"sort"
 	"strconv"
-
+	"time"
 )
 
 type Horse struct {
@@ -43,15 +43,11 @@ func playGame(balance float64, bet float64) float64{
 		return horses[i].time < horses[j].time
 	})
 
-	for i, horse := range horses {
-		fmt.Println(list[i], ". ", horse.name, horse.time)
-	}
-
-	fmt.Println("\nWhich one of Horsey's Cousins would you like to bet on?")
+	fmt.Println("\nWhich one of Horsey's Cousins would you like to bet on?\n")
 	for i := 0; i < len(horses); i++ {
 		fmt.Println(horses[i].name)
 	}
-
+	fmt.Println("")
 	var chosenHorse string
 	fmt.Scanln(&chosenHorse)
 	
@@ -67,34 +63,59 @@ func playGame(balance float64, bet float64) float64{
 	if checkingChosenHorse == 5 {
 		fmt.Println("Thats not an option, try again.")
 	} else {
-		fmt.Println("You have chosen: ", chosenHorse)
+		fmt.Println("\nYou have chosen: ", chosenHorse, "\n")
 	
-		if (chosenHorse == horses[0].name) {
+		if (chosenHorse == horses[4].name) {
 			balance += bet * 2
-		} else if (chosenHorse == horses[1].name) {
-			balance += bet 
 		} else if (chosenHorse == horses[3].name) {
+			balance += bet 
+		} else if (chosenHorse == horses[1].name) {
 			balance -= bet 
-		} else if (chosenHorse == horses[4].name) {
+		} else if (chosenHorse == horses[0].name) {
 			balance -= bet * 2
 		}
+	}
+
+	for i, horse := range horses {
+		fmt.Println(list[i], ". ", horse.name, horse.time)
+		time.Sleep(1 * time.Second)
+	}
+
+	switch chosenHorse {
+		case horses[0].name:
+			fmt.Printf("%s %s %.2f", "\nYou've Lost: ", "$", bet * 2)
+		case horses[1].name:
+			fmt.Printf("%s %s %.2f", "\nYou've Lost: ", "$", bet)
+		case horses[2].name:
+			fmt.Println("\nYou have not gain nor lost any money.")
+		case horses[3].name: 
+			fmt.Printf("%s %s %.2f", "\nYou've Won: ", "$", bet)
+		case horses[4].name: 
+			fmt.Printf("%s %s %.2f", "\nYou've Won: ", "$", bet * 2)
+			
+		}
+
+	if (balance <= 0) {
+		fmt.Printf("%s %.2f %s","\nYou have a balance of: ", balance, "\nYou can't place any more bets.")
 	}
 	return balance
 }
 
 func main() {
-	balance := 100.0
+	balance := 100.0	
+
+
 
 	for balance > 0 {
 		var input string
 		fmt.Println("")
 		fmt.Printf("Your balance is: $%.2f ", balance) 
-		fmt.Println("Enter your bet (q to quit): ") 
+		fmt.Println("Enter your bet (q to quit): \n") 
 		
 		fmt.Scanln(&input)
 
 		if input == "q"{
-			fmt.Println("Bye!!")
+			fmt.Println("You've left with: $", balance)
 			break
 		}
 
